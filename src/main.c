afcceabcda934015f1926cadae4540b2bd429e60
Maria N. V. Borges <m.nazare.borges.cc@gmail.com>
2026-01-13 20:54:04 -0300

    Create main.c

---
+++ src/main.c
@@ -0,0 +1,44 @@
+#include <stdio.h>
+
+#define SIZE 8
+
+void displayBoard(char board[SIZE][SIZE]) {
+    int row, col;
+    
+    printf("\n  a b c d e f g h\n");
+    for (row = 0; row < SIZE; row++) {
+        printf("%d ", 8 - row);
+        for (col = 0; col < SIZE; col++) {
+            printf("%c ", board[row][col]);
+        }
+        printf("%d\n", 8 - row);
+    }
+    printf("  a b c d e f g h\n\n");
+}
+
+int main()
+{
+    char board[SIZE][SIZE] = {
+        {'r','n','b','q','k','b','n','r'},
+        {'p','p','p','p','p','p','p','p'},
+        {'.','.','.','.','.','.','.','.'},
+        {'.','.','.','.','.','.','.','.'},
+        {'.','.','.','.','.','.','.','.'},
+        {'.','.','.','.','.','.','.','.'},
+        {'P','P','P','P','P','P','P','P'},
+        {'R','N','B','Q','K','B','N','R'}
+    };
+    
+    displayBoard(board);
+
+    return 0;
+}
+
+
+
+
+
+
+
+
+
